8e43ba38f5042074c081410cfbde3bbafb5de242
JorgeIsur <ramirezisur@gmail.com>
2021-12-19 18:05:51 -0600

    tiempo

--- PyMLX90614-0.0.3/mlx90614.py
+++ PyMLX90614-0.0.3/mlx90614.py
@@ -1,8 +1,10 @@
 from smbus2 import SMBus
 from mlx90614 import MLX90614
+import time
 bus = SMBus(1)
 sensor = MLX90614(bus, address=0x5A)
 print ("Ambient Temperature :"), sensor.get_ambient()
 while(True):
     print("Object Temperature :"), sensor.get_object_1()
+    time.sleep(3)
 bus.close()
