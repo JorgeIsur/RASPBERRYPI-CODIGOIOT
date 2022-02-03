132fb57ccf34e661e062690871856f1e4d7f7c77
Jorge Isur <ramirezisur@gmail.com>
2022-02-03 17:02:02 -0600

    Código

--- PyMLX90614-0.0.3/mlx90614.py
+++ PyMLX90614-0.0.3/mlx90614.py
@@ -4,17 +4,18 @@ from mlx90614 import MLX90614
 import time
 bus = SMBus(1)
 sensor = MLX90614(bus, address=0x5A)
-print ("Ambient Temperature :"), sensor.get_ambient()
-print ("Temperatura del objeto :", sensor.get_object_1())
+print ("Ambient Temperature :"+str(sensor.get_ambient()))
+print ("Temperatura del objeto :"+str(sensor.get_object_1()))
 def on_connect(client, userdata, flags, rc):
     print(f"Conectado con codigo de resultado {rc}")
 
 client = mqtt.Client()
 client.on_connect = on_connect
-client.connect("192.168.1.72", 1883, 60)
+client.connect("192.168.1.78", 1883, 60)
 while(1):
-    client.publish('isur/temperatura_salon', payload = sensor.get_ambient(),qos=0,retain=false)
-    client.publish('isur/temp',payload = sensor.get_object_1(),qos=0,retain=false)
+    print("Temperatura ambiente:"+str(sensor.get_ambient()))
+    print("temperatura objeto:"+str(sensor.get_object_1()))
+    client.publish('isur/temperatura_salon', payload = sensor.get_ambient(),qos=0,retain=False)
+    client.publish('isur/temp',payload = sensor.get_object_1(),qos=0,retain=False)
     time.sleep(5)
-    client.loop_forever()
 bus.close()
